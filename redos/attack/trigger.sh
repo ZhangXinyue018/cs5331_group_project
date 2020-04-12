3a021588bef85f8b08f1072d4c792e49ad61eee3
mercybaba <62169579+mercybaba@users.noreply.github.com>
2020-04-12 16:07:39 +0800

    Update trigger.sh

--- redos/attack/trigger.sh
+++ redos/attack/trigger.sh
@@ -1,6 +1,31 @@
-#! /bin/bash
+#!/bin/bash
 
-while true; do
- ./attack.sh 
- echo `date`
-done
+echo Hello, which script you want to trigger?
+
+# read script name
+read script_name
+
+
+if [ "$script_name" == "attack" ]
+
+then
+	
+# trigger attack script
+	echo triggering attack script
+
+	while true; do
+		./attack.sh 
+		echo `date`
+	done
+
+else
+
+# trigger solution script
+	echo triggering solution script
+
+	while true; do
+		./solution.sh 
+		echo `date`
+	done
+
+fi
